234e13540b2ff1e9ef439c36c013fd35341d59ac
MartaInes <73229109+MartaInes@users.noreply.github.com>
2022-05-12 17:48:27 +0100

    Add files via upload

--- src/main/java/aiss/api/resources/CustomCardResource.java
+++ src/main/java/aiss/api/resources/CustomCardResource.java
@@ -35,139 +35,139 @@ import aiss.model.repository.SpreadRepository;
 @Path("/custom")
 public class CustomCardResource {
 
-	public static CustomCardResource _instance=null;
-	static SpreadRepository repository;
+public static CustomCardResource _instance=null;
+static SpreadRepository repository;
+
+private CustomCardResource(){
+	repository=MapSpreadRepository.getInstance();
+}
+
+public static CustomCardResource getInstance()
+{
+	if(_instance==null)
+		_instance=new CustomCardResource();
+	return _instance; 
+}
+
+
+@GET
+@Produces("application/json")
+public static Collection<CustomCard> getAll(@QueryParam("order") String order, @QueryParam("suit") String suit, @QueryParam("name") String name)
+{
+    List<CustomCard> result = new ArrayList<CustomCard>();
+
+    for (CustomCard card: repository.getAllCustomCards()) {
+        if (suit != null && name == null) {
+            if (card.getSuit().toString().equals(suit)) {
+                result.add(card);
+            } 
+        }
+        if (name != null && suit == null) {
+        	if (card.getName().contains(name)) {
+        		result.add(card);
+        		break;
+        	}
+        }
+        if (name != null && suit != null) {
+        	throw new NotFoundException("Use one parameter only");	
+        }
+    }
+    
+    if (suit==null && name==null) {
+    	result = repository.getAllCustomCards().stream().collect(Collectors.toList());
+    }
+    
+    
+    if (order != null) {
+    	if (order.equals("name")) {
+            Collections.sort(result, new ComparatorNameCustom());
+        } 
+    	if (order.equals("-name")) {
+            Collections.sort(result, new ComparatorNameReversedCustom());
+        } 
+    }
+    
+    return result;
+}
+
+
+@GET
+@Path("/{id}")
+@Produces("application/json")
+public CustomCard get(@PathParam("id") String cardId)
+{
+	CustomCard card = repository.getCustomCard(cardId);
 	
-	private CustomCardResource(){
-		repository=MapSpreadRepository.getInstance();
+	if (card == null) {
+		throw new NotFoundException("The card with id="+ cardId +" was not found");			
 	}
 	
-	public static CustomCardResource getInstance()
-	{
-		if(_instance==null)
-			_instance=new CustomCardResource();
-		return _instance; 
+	return card;
+}
+
+
+@POST
+@Consumes("application/json")
+@Produces("application/json")
+public Response addCustomCard(@Context UriInfo uriInfo, CustomCard card) {
+	
+	if (card.getName() == null || "".equals(card.getName()))
+		throw new BadRequestException("The name of the card must not be null");
+
+	repository.addCustomCard(card);
+
+	// Builds the response. Returns the Card's uri the has just been added.
+	UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+	URI uri = ub.build(card.getId());
+	ResponseBuilder resp = Response.created(uri);
+	resp.entity(card);		
+	
+	return resp.build();
+}
+
+
+@PUT
+@Consumes("application/json")
+public Response updateCustomCard(CustomCard card) {
+	
+	CustomCard oldCard = repository.getCustomCard(card.getId());
+	
+	if (oldCard == null) {
+		throw new NotFoundException("The card with id="+ card.getId() +" was not found");			
 	}
 	
+	if (card.getId() == null)
+		throw new BadRequestException("The card must have an id.");
 	
-	@GET
-	@Produces("application/json")
-	public static Collection<CustomCard> getAll(@QueryParam("order") String order, @QueryParam("suit") String suit, @QueryParam("name") String name)
-	{
-	    List<CustomCard> result = new ArrayList<CustomCard>();
+	// Update name
+	if (card.getName()!=null)
+		oldCard.setName(card.getName());
 	
-	    for (CustomCard card: repository.getAllCustomCards()) {
-	        if (suit != null && name == null) {
-	            if (card.getSuit().equals(suit)) {
-	                result.add(card);
-	            } 
-	        }
-	        if (name != null && suit == null) {
-	        	if (card.getName().contains(name)) {
-	        		result.add(card);
-	        		break;
-	        	}
-	        }
-	        if (name != null && suit != null) {
-	        	throw new NotFoundException("Use one parameter only");	
-	        }
-	    }
-	    
-	    if (suit==null && name==null) {
-	    	result = repository.getAllCustomCards().stream().collect(Collectors.toList());
-	    }
-	    
-	    
-	    if (order != null) {
-	    	if (order.equals("name")) {
-	            Collections.sort(result, new ComparatorNameCustom());
-	        } 
-	    	if (order.equals("-name")) {
-	            Collections.sort(result, new ComparatorNameReversedCustom());
-	        } 
-	    }
-	    
-	    return result;
-	}
+	// Update suit
+	if (card.getSuit()!=null)
+		oldCard.setSuit(card.getSuit());
 	
+	// Update upright
+	if (card.getUpright()!=null)
+		oldCard.setUpright(card.getUpright());
 	
-	@GET
-	@Path("/{id}")
-	@Produces("application/json")
-	public CustomCard get(@PathParam("id") String cardId)
-	{
-		CustomCard card = repository.getCustomCard(cardId);
-		
-		if (card == null) {
-			throw new NotFoundException("The card with id="+ cardId +" was not found");			
-		}
-		
-		return card;
-	}
+	// Update reversed
+	if (card.getReversed()!=null)
+		oldCard.setReversed(card.getReversed());
+
+	return Response.noContent().build();
+}
+
+@DELETE
+@Path("/{id}")
+public Response removeCustomCard(@PathParam("id") String cardId) {
+	CustomCard Cardremoved = repository.getCustomCard(cardId);
 	
+	if (Cardremoved == null)
+		throw new NotFoundException("The card with id="+ cardId +" was not found");
+	else
+		repository.deleteCustomCard(cardId);
 	
-	@POST
-	@Consumes("application/json")
-	@Produces("application/json")
-	public Response addCustomCard(@Context UriInfo uriInfo, CustomCard card) {
-		
-		if (card.getName() == null || "".equals(card.getName()))
-			throw new BadRequestException("The name of the card must not be null");
-	
-		repository.addCustomCard(card);
-	
-		// Builds the response. Returns the Card's uri the has just been added.
-		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
-		URI uri = ub.build(card.getId());
-		ResponseBuilder resp = Response.created(uri);
-		resp.entity(card);		
-		
-		return resp.build();
-	}
-	
-	
-	@PUT
-	@Consumes("application/json")
-	public Response updateCustomCard(CustomCard card) {
-		
-		CustomCard oldCard = repository.getCustomCard(card.getId());
-		
-		if (oldCard == null) {
-			throw new NotFoundException("The card with id="+ card.getId() +" was not found");			
-		}
-		
-		if (card.getId() == null)
-			throw new BadRequestException("The card must have an id.");
-		
-		// Update name
-		if (card.getName()!=null)
-			oldCard.setName(card.getName());
-		
-		// Update suit
-		if (card.getSuit()!=null)
-			oldCard.setSuit(card.getSuit());
-		
-		// Update upright
-		if (card.getUpright()!=null)
-			oldCard.setUpright(card.getUpright());
-		
-		// Update reversed
-		if (card.getReversed()!=null)
-			oldCard.setReversed(card.getReversed());
-	
-		return Response.noContent().build();
-	}
-	
-	@DELETE
-	@Path("/{id}")
-	public Response removeCustomCard(@PathParam("id") String cardId) {
-		CustomCard Cardremoved = repository.getCustomCard(cardId);
-		
-		if (Cardremoved == null)
-			throw new NotFoundException("The card with id="+ cardId +" was not found");
-		else
-			repository.deleteCustomCard(cardId);
-		
-		return Response.noContent().build();
-	}
+	return Response.noContent().build();
+}
 }
