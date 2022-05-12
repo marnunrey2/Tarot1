adf02bcc712b8bc29ffec413a256c009a8b88e8f
Maria Nuñez <marnunrey2@alum.us.es>
2022-05-12 18:33:14 +0200

    c

--- src/main/java/aiss/api/resources/GameResource.java
+++ src/main/java/aiss/api/resources/GameResource.java
@@ -36,171 +36,171 @@ import aiss.model.repository.SpreadRepository;
 @Path("/game")
 public class GameResource {
 
-public static GameResource _instance=null;
-static SpreadRepository repository;
-
-private GameResource(){
-	repository=MapSpreadRepository.getInstance();
-}
-
-public static GameResource getInstance()
-{
-	if(_instance==null)
-		_instance=new GameResource();
-	return _instance; 
-}
-
-
-@GET
-@Produces("application/json")
-public static Collection<Game> getAll(@QueryParam("order") String order, @QueryParam("players") Integer players,@QueryParam("level") String level, @QueryParam("origin") String origin, @QueryParam("name") String name)
-{
-    List<Game> result = new ArrayList<Game>();
-
-    for (Game game: repository.getAllGames()) {
-        if (level != null && name == null) {
-            if (game.getDifficulty().toString().equals(level)) {
-                result.add(game);
-            } 
-        }
-        
-        if (origin != null && name == null) {
-            if (game.getOrigin().equals(origin)) {
-                result.add(game);
-            } 
-        }
-        
-        if (players != null && name == null) {
-            if (game.getNumPlayers().equals(players)) {
-                result.add(game);
-            } 
-        }
-        
-        if (name != null && level == null && origin == null) {
-        	if (game.getName().contains(name)) {
-        		result.add(game);
-        		break;
-        	}
-        }
-        
-        if (name != null && level != null || name != null && origin != null) {
-        	throw new NotFoundException("Name parameter must be used alone");	
-        }
-    }
-    
-    if (level==null && name==null && origin==null) {
-    	result = repository.getAllGames().stream().collect(Collectors.toList());
-    }
-    
-    
-    if (order != null) {
-    	if (order.equals("name")) {
-            Collections.sort(result, new ComparatorGameName());
-        } 
-    	if (order.equals("-name")) {
-            Collections.sort(result, new ComparatorGameNameReversed());
-        }
-    	if (order.equals("origin")) {
-            Collections.sort(result, new ComparatorGameOrigin());
-        } 
-    	if (order.equals("-origin")) {
-            Collections.sort(result, new ComparatorGameOriginReversed());
-        } 
-    }
-    
-    return result;
-}
-
-
-@GET
-@Path("/{id}")
-@Produces("application/json")
-public Game get(@PathParam("id") String gameId)
-{
-	Game game = repository.getGame(gameId);
+	public static GameResource _instance=null;
+	static SpreadRepository repository;
 	
-	if (game == null) {
-		throw new NotFoundException("The game with id="+ gameId +" was not found");			
+	private GameResource(){
+		repository=MapSpreadRepository.getInstance();
 	}
 	
-	return game;
-}
-
-
-@POST
-@Consumes("application/json")
-@Produces("application/json")
-public Response addGame(@Context UriInfo uriInfo, Game game) {
-	
-	if (game.getName() == null || "".equals(game.getName()))
-		throw new BadRequestException("The name of the card must not be null");
-
-	repository.addGame(game);
-
-	// Builds the response. Returns the Card's uri the has just been added.
-	UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
-	URI uri = ub.build(game.getId());
-	ResponseBuilder resp = Response.created(uri);
-	resp.entity(game);		
-	
-	return resp.build();
-}
-
-
-@PUT
-@Consumes("application/json")
-public Response updateGame(Game game) {
-	
-	Game oldGame = repository.getGame(game.getId());
-	
-	if (oldGame == null) {
-		throw new NotFoundException("The game with id="+ game.getId() +" was not found");			
+	public static GameResource getInstance()
+	{
+		if(_instance==null)
+			_instance=new GameResource();
+		return _instance; 
 	}
 	
-	if (game.getId() == null)
-		throw new BadRequestException("The game must have an id.");
 	
-	// Update name
-	if (game.getName()!=null)
-		oldGame.setName(game.getName());
+	@GET
+	@Produces("application/json")
+	public static Collection<Game> getAll(@QueryParam("order") String order, @QueryParam("players") Integer players,@QueryParam("level") String level, @QueryParam("origin") String origin, @QueryParam("name") String name)
+	{
+	    List<Game> result = new ArrayList<Game>();
 	
-	// Update origin
-	if (game.getOrigin()!=null)
-		oldGame.setOrigin(game.getOrigin());
+	    for (Game game: repository.getAllGames()) {
+	        if (level != null && name == null) {
+	            if (game.getDifficulty().toString().equals(level)) {
+	                result.add(game);
+	            } 
+	        }
+	        
+	        if (origin != null && name == null) {
+	            if (game.getOrigin().equals(origin)) {
+	                result.add(game);
+	            } 
+	        }
+	        
+	        if (players != null && name == null) {
+	            if (game.getNumPlayers().equals(players)) {
+	                result.add(game);
+	            } 
+	        }
+	        
+	        if (name != null && level == null && origin == null) {
+	        	if (game.getName().contains(name)) {
+	        		result.add(game);
+	        		break;
+	        	}
+	        }
+	        
+	        if (name != null && level != null || name != null && origin != null) {
+	        	throw new NotFoundException("Name parameter must be used alone");	
+	        }
+	    }
+	    
+	    if (level==null && name==null && origin==null) {
+	    	result = repository.getAllGames().stream().collect(Collectors.toList());
+	    }
+	    
+	    
+	    if (order != null) {
+	    	if (order.equals("name")) {
+	            Collections.sort(result, new ComparatorGameName());
+	        } 
+	    	if (order.equals("-name")) {
+	            Collections.sort(result, new ComparatorGameNameReversed());
+	        }
+	    	if (order.equals("origin")) {
+	            Collections.sort(result, new ComparatorGameOrigin());
+	        } 
+	    	if (order.equals("-origin")) {
+	            Collections.sort(result, new ComparatorGameOriginReversed());
+	        } 
+	    }
+	    
+	    return result;
+	}
 	
-	// Update difficulty
-	if (game.getDifficulty()!=null)
-		oldGame.setDifficulty(game.getDifficulty());
 	
-	// Update numPlayers
-		if (game.getNumPlayers()!=null)
-			oldGame.setNumPlayers(game.getNumPlayers());
-	
-	// Update numCards
-	if (game.getNumCards()!=null)
-		oldGame.setNumCards(game.getNumCards());
-	
-	// Update deck description
-		if (game.getDeckDescription()!=null)
-			oldGame.setDeckDescription(game.getDeckDescription());
+	@GET
+	@Path("/{id}")
+	@Produces("application/json")
+	public Game get(@PathParam("id") String gameId)
+	{
+		Game game = repository.getGame(gameId);
 		
-	// Update rules
-			if (game.getRules()!=null)
-				oldGame.setRules(game.getRules());
-
-	return Response.noContent().build();
-}
-
-@DELETE
-@Path("/{id}")
-public Response removeGame(@PathParam("id") String gameId) {
-	Game gameRemoved = repository.getGame(gameId);
+		if (game == null) {
+			throw new NotFoundException("The game with id="+ gameId +" was not found");			
+		}
+		
+		return game;
+	}
 	
-	if (gameRemoved == null)
-		throw new NotFoundException("The game with id="+ gameId +" was not found");
-	else
-		repository.deleteGame(gameId);
 	
-	return Response.noContent().build();
-}
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response addGame(@Context UriInfo uriInfo, Game game) {
+		
+		if (game.getName() == null || "".equals(game.getName()))
+			throw new BadRequestException("The name of the card must not be null");
+	
+		repository.addGame(game);
+	
+		// Builds the response. Returns the Card's uri the has just been added.
+		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+		URI uri = ub.build(game.getId());
+		ResponseBuilder resp = Response.created(uri);
+		resp.entity(game);		
+		
+		return resp.build();
+	}
+	
+	
+	@PUT
+	@Consumes("application/json")
+	public Response updateGame(Game game) {
+		
+		Game oldGame = repository.getGame(game.getId());
+		
+		if (oldGame == null) {
+			throw new NotFoundException("The game with id="+ game.getId() +" was not found");			
+		}
+		
+		if (game.getId() == null)
+			throw new BadRequestException("The game must have an id.");
+		
+		// Update name
+		if (game.getName()!=null)
+			oldGame.setName(game.getName());
+		
+		// Update origin
+		if (game.getOrigin()!=null)
+			oldGame.setOrigin(game.getOrigin());
+		
+		// Update difficulty
+		if (game.getDifficulty()!=null)
+			oldGame.setDifficulty(game.getDifficulty());
+		
+		// Update numPlayers
+			if (game.getNumPlayers()!=null)
+				oldGame.setNumPlayers(game.getNumPlayers());
+		
+		// Update numCards
+		if (game.getNumCards()!=null)
+			oldGame.setNumCards(game.getNumCards());
+		
+		// Update deck description
+			if (game.getDeckDescription()!=null)
+				oldGame.setDeckDescription(game.getDeckDescription());
+			
+		// Update rules
+				if (game.getRules()!=null)
+					oldGame.setRules(game.getRules());
+	
+		return Response.noContent().build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response removeGame(@PathParam("id") String gameId) {
+		Game gameRemoved = repository.getGame(gameId);
+		
+		if (gameRemoved == null)
+			throw new NotFoundException("The game with id="+ gameId +" was not found");
+		else
+			repository.deleteGame(gameId);
+		
+		return Response.noContent().build();
+	}
 }
